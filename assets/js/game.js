c4e27bff85f1f2cee0289914a789fad3f64c9a86
sakau <sakau@usc.edu>
2022-03-25 16:14:58 -0700

    leanre var, console, function, etc.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log('This logs a string, good for leaving yourself a message');
+console.log(10 +10);
+console.log("our robot's name is " +playerName);
+function fight () {
+    window.alert("The fight has begun!");
+}
+//fight();
